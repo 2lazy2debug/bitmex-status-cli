9c431c2ebd9812aae51615a499757a3f4791ee4b
Manuel Cabras <cabrasm@outlook.com>
2019-04-08 20:15:05 +0200

    Polished main.go

--- main.go
+++ main.go
@@ -6,70 +6,13 @@ package main
 
 import (
 	"bitmex-status-cli/utils"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strconv"
-	"time"
 )
-
-//var c config.Conf
 
 const BITMEXREFLINK = "https://www.bitmex.com/register/1TTdTj"
 
-type bitcoinTicker struct {
-	High      string `json:"high"`
-	Last      string `json:"last"`
-	Timestamp string `json:"timestamp"`
-	Bid       string `json:"bid"`
-	Vwap      string `json:"vwap"`
-	Volume    string `json:"volume"`
-	Low       string `json:"low"`
-	Ask       string `json:"ask"`
-	Open      string `json:"open"`
-}
-
-func bitcoinToDollar() {
-	url := fmt.Sprintf("https://www.bitstamp.net/api/v2/ticker/btcusd")
-	client := http.Client{
-		Timeout: time.Second * 10,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	valueBTC := bitcoinTicker{}
-	err = json.Unmarshal(body, &valueBTC)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bitcoinPrice, _ = strconv.ParseFloat(valueBTC.Bid, 64)
-}
-
-var bitcoinPrice float64
-
-func init() {
-	//c.GetConf()
-	bitcoinToDollar()
-}
-
 func main() {
-	fmt.Println("%v", utils.GetPosition())
+	fmt.Println("Your actual BitMex position : \n", utils.GetPosition())
 
-	fmt.Println("%v", BITMEXREFLINK)
+	fmt.Println("Share this link with a friend if you want him to join BitMEX : \n", BITMEXREFLINK)
 }
